8c73cc057a72fa52e862560b9d876609fb7a69ba
casa <casa@192.168.2.5>
2016-03-31 21:34:38 -0430

    se incluyo la funcion de agregar uno o mas tours virtuales y se realizaron otros cambios menores necesarios para lograr implementar dicha funcion

--- ProyectoIngenieria/src/Modelo/ConjTourVirtuales.java
+++ ProyectoIngenieria/src/Modelo/ConjTourVirtuales.java
@@ -13,4 +13,29 @@ import java.util.ArrayList;
  */
 public class ConjTourVirtuales {
     private ArrayList<TourVirtual> ListaToursVirtuales;
+    
+    public ConjTourVirtuales() {
+        ListaToursVirtuales = new ArrayList<>();
+    }
+    public void addTourVirtual () {
+        ListaToursVirtuales.add(new TourVirtual());
+    }
+
+    public void agregarNombre (int Tour, String nombreTourActual) {
+        TourVirtual cambiarCJ;
+        cambiarCJ =  ListaToursVirtuales.get(Tour);
+        cambiarCJ.setNombre(nombreTourActual);
+        ListaToursVirtuales.set(Tour, cambiarCJ);
+    }
+    
+    public String getnombre (int Tour) {
+        return ListaToursVirtuales.get(Tour).getNombre();
+    }
+    
+    public void guardarPuntosInteresTour (int CJ , ArrayList<PuntoInteres> list) {
+        TourVirtual aCJ;
+        aCJ=ListaToursVirtuales.get(CJ);
+        aCJ.setListaPuntosInteres(list);
+        ListaToursVirtuales.set(CJ, aCJ);
+    }
 }
